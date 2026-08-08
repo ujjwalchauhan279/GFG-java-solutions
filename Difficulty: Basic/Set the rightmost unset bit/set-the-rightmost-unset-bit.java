4ad78981fff920b0ef4c80e5933e13c1f64c1b79
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-08 21:04:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.java
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.java
@@ -1,18 +1,7 @@
 class Solution {
     public int setBit(int n) {
         // code here
-        int idx = 0;
-        int x = n;
-        while(x != 0){
-            int rightBit = (x & 1);
-            if(rightBit == 0){
-                break;
-            }
-            idx++;
-            x = x >> 1;
-        }
-        
-        return (n | (1 << idx));
+        return (n | (n+1));
         
     }
 }
